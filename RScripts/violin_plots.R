5319dc20b88b8aeaea21afe00158e50c831bab7d
Graeme T. Lloyd <graemetlloyd@gmail.com>
2019-08-31 10:50:13 +0100

    Updated violin plots script.

--- RScripts/violin_plots.R
+++ RScripts/violin_plots.R
@@ -1,15 +1,13 @@
 # Violin Plots
 
-files <- list.files(path="Data/StratCongruenceResults/InputTreeResults", pattern="*.txt", full.names=TRUE, recursive=FALSE)
+files <- list.files(path = "Data/StratCongruenceResults/InputTreeResults", pattern = "*.txt", full.names = TRUE, recursive = FALSE)
 
-lapply(files, makeViolins)
-
-  
-makeViolins <- function(x){
-  fi <- read.table(x, header=TRUE, sep=",")
+makeViolins <- function(x) {
+  fi <- read.table(x, header = TRUE, sep = " ")
   p <- ggplot(fi, aes(factor(Bayes), MIG))
-  q <- p + geom_violin(scale = "count")+ geom_jitter(height = 0, width = 0.2, alpha = .4)
-  new_filename <- paste0(x, '.pdf')
+  q <- p + geom_violin(scale = "count") + geom_jitter(height = 0, width = 0.2, alpha = .4)
+  new_filename <- gsub("StratCongruenceResults/InputTreeResults/", "ViolinPlots/", gsub(".txt", ".pdf", x))
   ggsave(new_filename, q)
 }
-  
+
+lapply(files, makeViolins)
