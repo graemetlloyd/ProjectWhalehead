766f176788a421204db0595879b11e83637c4122
April Wright <wright.aprilm@gmail.com>
2020-01-31 15:00:07 -0600

    adding SPR function

---
+++ R/spr_tree.dist.matrix.R
@@ -0,0 +1,43 @@
+#' Tree distance matrix calculation
+#' 
+#' This function takes a list of trees and returns a distance matrix
+#' populated with Robinson-Foulds tree distances between all trees
+#' in the list.
+#'
+#' @param trees a multiPhylo object 
+#'
+#' @return RF A distance matrix of RF distances 
+#'
+#' @keywords treespace, tree distance, robinson-foulds
+#'
+#' @export tree.dist.matrix
+#' @examples
+#' \dontrun{
+#' data(fungus)
+#' tree.dist.matrix(fungus$Fungus.Run1$trees)
+#' }
+
+cust_tree.dist.matrix <- function(trees){
+    if (class(trees) != "multiPhylo") 
+        stop("trees should be an object of class \"multiPhylo\"")
+
+    N <- length(trees)
+
+    treenames <- 1:N
+    
+    # use the RF.dist function from Phangorn. Thanks to Klaus Vigo for pointing
+    # out that we were doing this all wrong before!
+    # https://github.com/danlwarren/RWTY/issues/47
+    SPR <- as.matrix(phangorn::SPR.dist(trees))
+
+    #Row and column names
+    rownames(SPR) <- treenames
+    colnames(SPR) <- treenames
+    
+    SPR
+}
+
+
+
+
+        
